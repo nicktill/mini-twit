288e71c5d0f7049da7f80f29a6b945d8fdd76a5b
Nicholas Tillmann <ntillmann1439@gmail.com>
2022-12-05 01:56:33 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,14 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Loader from '../components/Loader'
+import toast from 'react-hot-toast';
 
 export default function Home() {
-    return (
-        <div>
-           <Loader show  />
-        </div>
-    )
+  return (
+    <div>
+      <button onClick={() => toast.success('tweeted!')}>
+        tweet 
+      </button>
+    </div>
+  );
 }
